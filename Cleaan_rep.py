88ae54c222d82c2201b602e488d890faeb86e8ee
Extro <64098846+ExtroZ-ui@users.noreply.github.com>
2024-09-25 15:27:14 +0500

    Script add

---
+++ Cleaan_rep.py
@@ -0,0 +1,16 @@
+import os
+import subprocess
+import sys
+import msvcrt
+
+if not os.path.exists('.git'):
+    print('Скрипт должен выполняться в корневом каталоге Git-репозитория.')
+    sys.exit(1)
+
+# Очищаем 
+subprocess.call(["git", "reset", "--hard"])
+subprocess.call(["git","clean","-fdx"])
+
+print('Очистка выполнена')
+
+
---
+++ Merge_rep.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+import sys
+import msvcrt
+
+if not os.path.exists('.git'):
+    print('Скрипт должен выполняться в корневом каталоге Git-репозитория.')
+    sys.exit(1)
+
+# Перенос ревизии из ветки dev в prd
+subprocess.run(["git", "checkout", "prd"])
+subprocess.run(["git", "merge", "--no-ff", "dev"])
+
+# Установка тега
+subprocess.run(["git", "tag", "-a", "v1.0", "-m", "тест сообщения"])
+
+# Отправка изменений
+subprocess.run(["git", "push", "origin", "prd", "--tag"])
+
+print('Ветка dev в prd объединилась с установкой тэга')
+
