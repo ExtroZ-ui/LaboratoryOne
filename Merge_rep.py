b1a3863b65523bb1d194df8403ad7eaf8d46770c
Extro <64098846+ExtroZ-ui@users.noreply.github.com>
2024-09-25 15:45:38 +0500

    Fix

---
+++ Merge_rep.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+import sys
+import msvcrt
+
+if not os.path.exists('.git'):
+    print('Скрипт должен выполняться в корневом каталоге Git-репозитория.')
+    sys.exit(1)
+
+# Перенос ревизии из ветки dev в prd
+subprocess.run(["git", "checkout", "prd"])
+subprocess.run(["git", "merge", "--no-ff", "dev"])
+
+print('Введите название тега')
+new_tag = input()
+# Установка тега
+subprocess.run(["git", "tag", "-a", f"{new_tag}", "-m", "тест сообщения"])
+
+# Отправка изменений
+subprocess.run(["git", "push", "origin", "prd", "--tag"])
+
+print('Ветка dev в prd объединилась с установкой тэга\n Для завершения нажмите любую клавишу')
+msvcrt.getche()
